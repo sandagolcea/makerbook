bf3a6aa93a85295f9491add27422f4827b8f6903
Ptolemy <ptolemybarnes@gmail.com>
2015-01-29 14:25:21 +0000

    Pass test for password prompt.

--- spec/features/userservices_features.js
+++ spec/features/userservices_features.js
@@ -1,28 +1,24 @@
 describe('New user page', function(){
   before(function() {
-    casper.start('http://localhost:3000/');
+    casper.start('http://localhost:3000/user/new'); // tells the browser where to navigate to at the start.
   });
 
   describe('prompts user for', function() {
 
-    before(function() {
-      casper.open("http://localhost:3000/user/new");
-    });
-
-    it('prompts user for username', function(){
+    it('username', function(){
 
       casper.then(function(){
         expect("body").to.include.text("Enter your github user name");
       });
     });
 
-    it('prompts user for username', function(){
+    it('password', function(){
 
       casper.then(function(){
         expect("body").to.include.text("Enter your github password");
       });
     });  
-    
+
   }) // end of tests for prompts.
 
 
